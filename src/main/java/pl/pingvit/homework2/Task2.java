b3891e38545e26f30d94e21f914f8802abb0b055
SavelevVadim <93464255+SavelevVadim@users.noreply.github.com>
2023-09-10 22:01:21 +0200

    add corrections

--- src/main/java/pl/pingvit/homework2/Task2.java
+++ src/main/java/pl/pingvit/homework2/Task2.java
@@ -5,19 +5,19 @@ public class Task2 {
         String myByteString = "77";
         // опечатка: myParsedByte, без буквы n. это касается и остальных переменных
         // переименуй из через refactor -> rename
-        Byte myParsendByte = Byte.parseByte(myByteString);
+        Byte myParsedByte = Byte.parseByte(myByteString);
         String myShortString = "12587";
-        Short myParsendShort = Short.parseShort(myShortString);
+        Short myParsedShort = Short.parseShort(myShortString);
         String myIntString = "-78945627";
-        Integer myParsendInt = Integer.parseInt(myIntString);
+        Integer myParsedInt = Integer.parseInt(myIntString);
         String myFloatString = "123.6789";
-        Float myParsenFloat = Float.parseFloat(myFloatString);
+        Float myParsedFloat = Float.parseFloat(myFloatString);
         String myDoubleString = "-8965.876655";
-        Double myParsendDouble = Double.parseDouble(myDoubleString);
-        System.out.println(myParsendByte);
-        System.out.println(myParsendShort);
-        System.out.println(myParsendInt);
-        System.out.println(myParsenFloat);
-        System.out.println(myParsendDouble);
+        Double myParsedDouble = Double.parseDouble(myDoubleString);
+        System.out.println(myParsedByte);
+        System.out.println(myParsedShort);
+        System.out.println(myParsedInt);
+        System.out.println(myParsedFloat);
+        System.out.println(myParsedDouble);
     }
 }
