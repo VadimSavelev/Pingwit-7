e0eba4a7ee00e75b46ba8760b57171d5a50caf74
SavelevVadim <93464255+SavelevVadim@users.noreply.github.com>
2023-09-18 17:01:11 +0200

    add task1 task2

---
+++ src/main/java/pl/pingvit/homework4/Butterfly.java
@@ -0,0 +1,42 @@
+package pl.pingvit.homework4;
+
+import java.util.Scanner;
+
+public class Butterfly {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter size butterfly : ");
+        int size = scanner.nextInt();
+        for (int i = 0; i <= size; i++) {
+            //верх
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
+            }
+            //промежуток
+            for (int j = 0; j <= 2 * (size - i); j++) {
+                System.out.print(" ");
+            }
+            //лево верх
+            for (int j = i; j >=0 ; j--) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+        for (int i = size; i >= 0; i--) {
+            //низ право
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
+            }
+            //низ промеж2
+            for (int j = 0; j <= 2 * (size - i); j++) {
+                System.out.print(" ");
+            }
+            //низ лево2
+            for (int j = i; j >=0 ; j--) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
+
