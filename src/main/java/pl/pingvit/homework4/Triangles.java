2aa9c0b39305f6c0136f7eb491dd787d3c5be69b
SavelevVadim <93464255+SavelevVadim@users.noreply.github.com>
2023-09-20 06:04:33 +0200

    add corect task Triangels

---
+++ src/main/java/pl/pingvit/homework4/Triangles.java
@@ -0,0 +1,38 @@
+package pl.pingvit.homework4;
+
+/*Нарисуйте оставшиеся 2 треугольника.
+        Опционально: можете нарисовать все 4 фигуры, чтобы у вас получился ромб
+        *  (1)
+        **
+        ***
+        ****
+
+        ****  (2)
+        ***
+        **
+        *
+*/
+public class Triangles {
+    public static void main(String[] args) {
+        int size=4;
+        for (int i = 0; i <= size; i += 1) {
+            for (int j = 0; j < size - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= size; i ++) {
+            for (int j = 0; j < i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < size - i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
