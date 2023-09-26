ec9e82f9d424e386f52f1114afd53896a64d6253
Pavel Radkevich <pradkevich@eisgroup.com>
2023-09-26 16:28:54 +0300

    ДЗ 5 проверка 1

--- src/main/java/pl/pingvit/homework5/ArrayOfEvenOnesFromTheFirst.java
+++ src/main/java/pl/pingvit/homework5/ArrayOfEvenOnesFromTheFirst.java
@@ -2,15 +2,16 @@ package pl.pingvit.homework5;
 import java.util.Arrays;
 //Есть массив чисел. Необходимо создать другой массив содержащий только четные элементы из первого
 public class ArrayOfEvenOnesFromTheFirst {
+    // нужно сделать форматирование кода
     public static void main(String[] args) {
-        int[] mas = {2, 3, 5, 7, 8, 4, 2};
-        int evenNums = 0;
+        int[] mas = {2, 3, 5, 7, 8, 4, 2}; // mas - имя переменной несет недостаточно информации. варианты: initial (изначальный), start, initialData
+        int evenNums = 0;  // в переменной хранится счетчик - предлагаю ей более точное имя, например evenNumsCounter
         for (int i = 0; i < mas.length; i++) {
             if (mas[i] % 2 == 0) {
                 evenNums++;
             }
         }
-        int[] mas2 = new int[evenNums];
+        int[] mas2 = new int[evenNums];  // mas2 - имя переменной несет недостаточно информации. варианты: result, evenNumbers
         int index = 0;
         for (int i = 0; i < mas.length; i++) {
             if (mas[i] % 2 == 0) {
